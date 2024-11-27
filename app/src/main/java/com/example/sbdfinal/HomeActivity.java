85d8525e957802ac8c3fa76ab1169775a565ea74
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2024-11-27 17:59:37 +0600

    Home Activity

--- app/src/main/java/com/example/sbdfinal/HomeActivity.java
+++ app/src/main/java/com/example/sbdfinal/HomeActivity.java
@@ -1,17 +1,62 @@
 package com.example.sbdfinal;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    DrawerLayout drawerid;
+    MaterialToolbar Mtoolbarid;
+    TextView tooltitel;
+    BottomNavigationView bottomnav;
+    NavigationView navigationid;
+
+    long backPressedTime;
+    Toast backPressedToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //====================== ID will define here ========================
+
+        tooltitel = findViewById(R.id.tooltitel);
+        Mtoolbarid = findViewById(R.id.Mtoolbarid);
+        drawerid = findViewById(R.id.drawerid);
+        bottomnav = findViewById(R.id.bottomnav);
+        navigationid = findViewById(R.id.navigationid);
+
+        //====================== ID will define here ========================
+
+
+        // Setup ActionBarDrawerToggle
+        setSupportActionBar(Mtoolbarid);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                HomeActivity.this, drawerid, Mtoolbarid, R.string.open, R.string.close);
+        drawerid.addDrawerListener(toggle);
+        //built in menu icon
+        toggle.syncState();
+        toggle.getDrawerArrowDrawable().setColorFilter(getResources().getColor(android.R.color.black), PorterDuff.Mode.SRC_ATOP);
+
 
     }
 
+
+
+
+
+
 }
+
--- app/src/main/java/com/example/sbdfinal/SplashScreen.java
+++ app/src/main/java/com/example/sbdfinal/SplashScreen.java
@@ -3,6 +3,10 @@ package com.example.sbdfinal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +17,20 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+
+        // Find your views
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView textView = findViewById(R.id.textView);
+
+        // Load animations
+        Animation slideTopToBottom = AnimationUtils.loadAnimation(this, R.anim.slide_top_to_bottom);
+
+        // Start animations
+        imageView.startAnimation(slideTopToBottom);
+        textView.startAnimation(slideTopToBottom);
+
+
+
 //=============== handle splash screen ==========================
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -22,9 +40,9 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
                 //slide in, out animation for activity
-
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
-        }, 3500);
+        }, 2750);
 //=============== handle splash screen ==========================
 
     }
