e33d2cb86adfe2c00f839fa6a6a6cb726787d64a
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2024-11-27 12:47:14 +0600

    logo change

--- app/src/main/java/com/example/sbdfinal/SplashScreen.java
+++ app/src/main/java/com/example/sbdfinal/SplashScreen.java
@@ -4,11 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -22,7 +18,7 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
                 //slide in, out animation for activity
