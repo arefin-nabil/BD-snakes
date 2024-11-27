e37fe1e9ef82472c3d292df8bf3c5bb9255f897c
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2024-11-27 10:50:56 +0600

    splash ended

--- app/src/main/java/com/example/sbdfinal/SplashScreen.java
+++ app/src/main/java/com/example/sbdfinal/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.sbdfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +17,19 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        //  slash code here
+//=============== handle splash screen ==========================
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                //slide in, out animation for activity
+
+            }
+        }, 3500);
+//=============== handle splash screen ==========================
 
     }
 
